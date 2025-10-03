29ca586830b1d771ffb748f5f0f060fe851f7a92
danishmean <84640463+danishmean@users.noreply.github.com>
2025-10-03 18:24:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,10 +1,11 @@
 api_id = 28585377
 api_hash = "35f71aa74e9eddd4f57171d8d368953b"
-bot_token = "8451308009:AAH62H3vKzGYgwnlmnDhPIhq1gcP6BlyEGk"
+bot_token = "8451308009:AAFyzgiesjBpevyMh9tnXae-kDgiS_qOTNo"
 auth_users = [1713924419]
 sudo_user = 1713924419
 log_channel = -1002143629860
 txt_channel = -1002049391187
+
 
 
 
